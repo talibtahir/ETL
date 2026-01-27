dfa938b29b80104b03bc8b19bc371c368a57c183
talibtahir <talibtahir6876@gmail.com>
2026-01-27 16:44:43 +0530

    Remove Unwanted Files

--- robo.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-if __name__ == '__main__':
-    print("welcome to Robo Speaker 1.1 Created by Talib Iconic... ")
-    x = input("Enter What you want to speak: ")
-    command = (f"say {x}")
-    pyttsx3.system(command)
-    
